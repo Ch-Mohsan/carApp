32b0160e8e4e74cbb88b8e8521a54ed5f708ade2
s23bdocs1m01081-wq <s23bdocs1m01081@iub.edu.pk>
2025-12-09 18:53:18 +0500

    set base url

--- frontend/src/data/api.js
+++ frontend/src/data/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL = import.meta.env?.BASE_URL ;
+const baseURL = import.meta.env?.VITE_BASE_URL ;
 
 export const api = axios.create({ baseURL });
 api.interceptors.request.use((config) => {
