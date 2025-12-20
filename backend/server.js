f8178c0be5dc520ff2262148e8fe305f37d5d0b9
s23bdocs1m01081-wq <s23bdocs1m01081@iub.edu.pk>
2025-12-21 04:58:27 +0500

    set origan

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,12 @@ const bookingRoute = require('./routes/booking_auth');
 const port = 3000;
 const cors = require('cors');
 
-origin: [
+app.use(cors({
+  origin: [
     "http://localhost:5173",
     "https://your-frontend-vercel-url.vercel.app"
   ],
-
+}));
 app.use(express.json());
 // Ensure uploads directory exists and serve it statically
 try {
